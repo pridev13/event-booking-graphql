8da49d414b0d3c5be7c76c2c5f29417e22f18777
pridev13 <m@mvhr.nl>
2020-04-08 09:38:34 +0200

    Expanded GraphQL schema a bit

--- app.js
+++ app.js
@@ -5,18 +5,35 @@ const {buildSchema} = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
 	'/graphql',
 	graphqlHttp({
 		schema: buildSchema(`
+			type Event {
+				_id: ID!
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
+			input EventInput {
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
 			type RootQuery {
-				events: [String!]!
+				events: [Event!]!
 			}
 
 			type RootMutation {
-				createEvent(name: String): String
+				createEvent(eventInput: EventInput): Event
 			}
 
 			schema {
@@ -26,18 +43,21 @@ app.use(
 		`),
 		rootValue: {
 			events: () => {
-				return [
-					'Event 1',
-					'Event 2',
-					'Event 3',
-				]
+				return events;
 			},
 			createEvent: (args) => {
-				const eventName = args.name;
-				return eventName;
-			}
+				const event = {
+					_id: Math.random().toString(),
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: new Date().toISOString(),
+				};
+				events.push(event);
+				return event;
+			},
 		},
-		graphiql: true
+		graphiql: true,
 	})
 );
 
